7ff6a13bb0114293a3502db56eea87e8bee595db
Ravel Andrade <ravel.gou@gmail.com>
2021-02-10 13:23:34 -0300

    Inserção de usuário com POST

--- src/main/java/com/ravel/workshopmongo/Resources/UserResource.java
+++ src/main/java/com/ravel/workshopmongo/Resources/UserResource.java
@@ -6,13 +6,16 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ravel.workshopmongo.domain.User;
 import com.ravel.workshopmongo.dto.UserDTO;
 import com.ravel.workshopmongo.services.UserService;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -29,9 +32,17 @@ public class UserResource {
 	}
 	
 	@RequestMapping(value="/{id}",  method = RequestMethod.GET)
-	
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = service.findById(id);
-		return ResponseEntity.ok().body(new UserDTO(user));
+		return ResponseEntity.ok().body(new UserDTO());
+	}
+	
+@RequestMapping(method = RequestMethod.POST)
+	
+	public ResponseEntity<Void> Insert(@RequestBody UserDTO objDto) {
+		User user = service.fromDTO(objDto);
+		user = service.Insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/ravel/workshopmongo/services/UserService.java
+++ src/main/java/com/ravel/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ravel.workshopmongo.domain.User;
+import com.ravel.workshopmongo.dto.UserDTO;
 import com.ravel.workshopmongo.repository.UserRepository;
 import com.ravel.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User Insert(User obj) {
+		return repo.insert(obj);
+		
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
