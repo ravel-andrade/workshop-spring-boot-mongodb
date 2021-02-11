3e8b2b6fe7bca5ec0345aa9babbc73ff042bac6c
Ravel Andrade <ravel.gou@gmail.com>
2021-02-10 21:02:03 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ravel/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.ravel.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ravel.workshopmongo.domain.Post;
+import com.ravel.workshopmongo.repository.PostRepository;
+import com.ravel.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
